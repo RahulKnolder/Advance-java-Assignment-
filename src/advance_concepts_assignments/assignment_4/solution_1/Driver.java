5763aa975708e9a6cc0b7a8326cb25aacbd83531
ChilledCoder <rahul.sinha@knoldus.com>
2023-03-22 18:34:34 +0530

    refactorinf my code for the third time

--- src/advance_concepts_assignments/assignment_4/solution_1/Driver.java
+++ src/advance_concepts_assignments/assignment_4/solution_1/Driver.java
@@ -9,7 +9,7 @@ public class Driver {
     public static void main(String[] args) {
 
         // list input
-        List<String> myFirstList = new ArrayList();
+        List<String> firstList = new ArrayList();
         Scanner readItems = new Scanner(System.in);
 
         System.out.println("Enter the number of elements: ");
@@ -23,16 +23,16 @@ public class Driver {
             for (int indexOfList = 0; indexOfList < numberOfInputs; ++indexOfList) {
                 System.out.print("Enter element " + (indexOfList + 1) + ": ");
                 String myString = readItems.nextLine();
-                myFirstList.add(myString);
+                firstList.add(myString);
             }
 
            // using streams to perform operations nad store in ithor list
-            List<String> myDistinctList = myFirstList.stream().distinct().sorted().collect(Collectors.toList());
+            List<String> distinctList = firstList.stream().distinct().sorted().collect(Collectors.toList());
             System.out.println();
             System.out.println("Elements after finding the distinct elements");
 
 
-            for (String element : myDistinctList) {
+            for (String element : distinctList) {
                 System.out.print(element+" ");
             }
 
